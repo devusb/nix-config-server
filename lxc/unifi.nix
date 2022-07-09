a6a84822eade5404b8dc6fbd45c591a33e626739
Morgan Helton <mhelton@gmail.com>
2022-07-09 14:05:38 -0500

    add unifi backup

--- lxc/unifi.nix
+++ lxc/unifi.nix
@@ -12,6 +12,22 @@
     replaceUnknownProfiles = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    (deployBackup {
+      backup_name = "unifi";
+      backup_files_list = [
+        ''"$(find "/var/lib/unifi/data/backup/autobackup" -path "*autobackup*unf*" -printf '%Ts\t%p\n' | sort -n | cut -f2 | tail -n 1)"''
+      ];
+    })
+  ];
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 0 * * 1     root    deployBackup"
+    ];
+  };
+
   services.unifi = {
     enable = true;
     unifiPackage = pkgs.unifi6;
