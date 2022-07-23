1816835c5d606e7f600a35a66cf815f9be03035e
Morgan Helton <mhelton@gmail.com>
2022-07-23 15:04:20 -0500

    overlay pomerium to v0.17.3

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,4 +6,44 @@
       rm /tmp/${backup_name}.tar.gz
       logger "${backup_name} backup completed $(date)"
     '';
+
+  pomerium = prev.pomerium.overrideAttrs (old: rec {
+    version = "0.17.3";
+    src = prev.fetchFromGitHub {
+      owner = "pomerium";
+      repo = "pomerium";
+      rev = "v0.17.3";
+      sha256 = "sha256-p6bMFlvFbe9AXhujVHwf2zAxnN5b2ep7zbD1PQ4QKYQ=";
+    };
+    ldflags =
+      let
+        # Set a variety of useful meta variables for stamping the build with.
+        setVars = {
+          "github.com/pomerium/pomerium/internal/version" = {
+            Version = "v0.17.3";
+            BuildMeta = "nixpkgs";
+            ProjectName = "pomerium";
+            ProjectURL = "github.com/pomerium/pomerium";
+          };
+          "github.com/pomerium/pomerium/internal/envoy" = {
+            OverrideEnvoyPath = "${prev.envoy}/bin/envoy";
+          };
+        };
+        concatStringsSpace = list: prev.lib.concatStringsSep " " list;
+        mapAttrsToFlatList = fn: list: prev.lib.concatMap prev.lib.id (prev.lib.mapAttrsToList fn list);
+        varFlags = concatStringsSpace (
+          mapAttrsToFlatList
+            (package: packageVars:
+              prev.lib.mapAttrsToList
+                (variable: value:
+                  "-X ${package}.${variable}=${value}"
+                )
+                packageVars
+            )
+            setVars);
+      in
+      [
+        "${varFlags}"
+      ];
+  });
 }
