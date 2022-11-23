9d351255e23841a89690d42c6a46c4e6dcf31874
Morgan Helton <mhelton@gmail.com>
2022-11-23 17:12:43 -0600

    clean-up atuin defaultText, switch package option to mkPackageOption

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -10,45 +10,35 @@ in
     services.atuin = {
       enable = mkEnableOption (mdDoc "Enable server for shell history sync with atuin.");
 
-      package = mkOption {
-        type = types.package;
-        default = pkgs.atuin;
-        defaultText = literalExpression "pkgs.atuin";
-        description = "The package to use for atuin.";
-      };
+      package = mkPackageOption pkgs "atuin" { };
 
       openRegistration = mkOption {
         type = types.bool;
         default = true;
-        defaultText = literalExpression "true";
         description = mdDoc "Allow new user registrations with the atuin server.";
       };
 
       path = mkOption {
         type = types.str;
         default = "";
-        defaultText = literalExpression "";
         description = mdDoc "A path to prepend to all the routes of the server.";
       };
 
       host = mkOption {
         type = types.str;
         default = "127.0.0.1";
-        defaultText = literalExpression "127.0.0.1";
         description = mdDoc "The host address the atuin server should listen on.";
       };
 
       port = mkOption {
         type = types.ints.u16;
         default = 8888;
-        defaultText = literalExpression "8888";
         description = mdDoc "The port the atuin server should listen on.";
       };
 
       openFirewall = mkOption {
         type = types.bool;
         default = false;
-        defaultText = literalExpression "false";
         description = mdDoc "Open ports in the firewall for the atuin server.";
       };
 
