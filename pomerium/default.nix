8c6bb16a2550f61f7e536770be760954b9dc34d2
Morgan Helton <mhelton@gmail.com>
2022-12-04 08:37:33 -0600

    add redis persistency, remove code-server from pomerium routes, fix fly concurrency issues

--- pomerium/default.nix
+++ pomerium/default.nix
@@ -1,15 +1,18 @@
 { pkgs, ... }:
 let
   pomeriumConfig = pkgs.writeText "config.yaml" (builtins.readFile ../aws-proxy/pomerium/config.yaml);
+  redisConfig = pkgs.writeText "redis.conf" ''
+    dir /data
+  '';
   entrypoint = pkgs.writeShellScriptBin "entrypoint.sh" ''
     # Create the tun device path if required
     if [ ! -d /dev/net ]; then mkdir /dev/net; fi
     if [ ! -e /dev/net/tun ]; then  mknod /dev/net/tun c 10 200; fi
-      
+
     # Wait 5s for the daemon to start and then run tailscale up to configure
     /bin/sh -c "${pkgs.coreutils}/bin/sleep 5; ${pkgs.tailscale}/bin/tailscale up --accept-routes --accept-dns --authkey=$TAILSCALE_AUTHKEY" &
-    exec ${pkgs.tailscale}/bin/tailscaled --state=/tailscale/tailscaled.state &
-    ${pkgs.redis}/bin/redis-server &
+    exec ${pkgs.tailscale}/bin/tailscaled --state=/data/tailscaled.state &
+    ${pkgs.redis}/bin/redis-server ${redisConfig} &
     ${pkgs.pomerium}/bin/pomerium -config "${pomeriumConfig}"
 
   '';
