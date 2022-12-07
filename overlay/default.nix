519c1f4e1349deb38df02aff72ce93c98314e577
Morgan Helton <mhelton@gmail.com>
2022-12-07 16:25:48 -0600

    upgrade tailscale-unstable to 1.34.0, start caddy before tailscaled to prevent blocking of :443

--- overlay/default.nix
+++ overlay/default.nix
@@ -3,18 +3,18 @@
 
   tailscale-unstable =
     let
-      version = "1.33.298";
+      version = "1.34.0";
       src = prev.fetchFromGitHub {
         owner = "tailscale";
         repo = "tailscale";
-        rev = "300aba61a6bdd51256f2a3d9453e5e459ed4cfc6";
-        sha256 = "sha256-igIUhU8Rv/1xmZmbskwfUXXjM2jp7dGjtkgcO5qPtB4=";
+        rev = "v${version}";
+        sha256 = "sha256-ngcFoEDec/6I9gWpJ767ju2OvZfS4RhlSbK//xXIFxs=";
       };
     in
     (prev.tailscale.override {
       buildGoModule = args: prev.buildGoModule.override { } (args // {
         inherit src version;
-        vendorSha256 = "sha256-fbRdC98V55KqzlkJRqTcjsqod4CUYL2jDgXxRmwvfSE=";
+        vendorSha256 = "sha256-nSllDi6G4QAGyuoGduDhI0vaVuN2//eg+gXRSZ3ERiQ=";
         ldflags = [ "-X tailscale.com/version.Long=${version}" "-X tailscale.com/version.Short=${version}" ];
       });
     });
