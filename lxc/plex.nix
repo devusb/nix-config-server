127124fef75421b97798f5277fed4fdb156454bd
Morgan Helton <mhelton@gmail.com>
2022-12-29 22:00:35 -0500

    switch plex lxc to overlay

--- lxc/plex.nix
+++ lxc/plex.nix
@@ -49,15 +49,7 @@ in
     user = "media";
     group = "media";
     dataDir = plexData;
-    package = (pkgs.plex.override {
-      plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-        version = "1.30.1.6497-5fc2e0894";
-        src = pkgs.fetchurl {
-          url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-          sha256 = "sha256-VwaetJED30ot62LIJi8Ix5IrIa7irbzdGFZbIqz3PgU";
-        };
-      });
-    });
+    package = pkgs.plexpass;
   };
 
   services.promtail = with lib; {
