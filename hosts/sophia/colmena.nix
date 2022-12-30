aa442de892ee51b08b9dea0ba918ec9989d3ee9f
Morgan Helton <mhelton@gmail.com>
2022-12-30 05:48:30 -0600

    use smaller prefixes for sophia subnet routes

--- hosts/sophia/colmena.nix
+++ hosts/sophia/colmena.nix
@@ -10,7 +10,7 @@
   # tailscale
   services.tailscale-autoconnect = {
     enable = true;
-    extraTailscaleArgs = [ "--advertise-exit-node" "--advertise-routes=192.168.0.0/16" "--accept-routes" "--accept-dns=false" ];
+    extraTailscaleArgs = [ "--advertise-exit-node" "--advertise-routes=192.168.10.0/23,192.168.20.0/23,192.168.30.0/24,192.168.99.0/24" "--accept-routes" "--accept-dns=false" ];
   };
 
 }
