9be677a49c408b3887a8a5b5157bf74813f06d83
Morgan Helton <mhelton@gmail.com>
2023-02-04 17:25:56 -0600

    upgrade plex to 1.31.0.6654-02189b09f

--- overlay/default.nix
+++ overlay/default.nix
@@ -5,10 +5,10 @@
 
   plexpass = prev.plex.override {
     plexRaw = prev.plexRaw.overrideAttrs (old: rec {
-      version = "1.30.1.6497-5fc2e0894";
+      version = "1.31.0.6654-02189b09f";
       src = prev.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "sha256-VwaetJED30ot62LIJi8Ix5IrIa7irbzdGFZbIqz3PgU";
+        sha256 = "sha256-TTEcyIBFiuJTNHeJ9wu+4o2ol72oCvM9FdDPC83J3Mc=";
       };
     });
   };
