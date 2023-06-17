69173916b897bf06d267aa510e1f14648e5f14e3
Morgan Helton <mhelton@gmail.com>
2023-06-17 15:32:53 -0500

    update zwave-js-ui

--- hosts/gaia/gaia0.nix
+++ hosts/gaia/gaia0.nix
@@ -21,7 +21,7 @@
 
   virtualisation.oci-containers.containers = {
     zwave-js-ui = {
-      image = "zwavejs/zwave-js-ui:8.6.3";
+      image = "zwavejs/zwave-js-ui:8.18.1";
       ports = [ "8091:8091" "3000:3000" ];
       volumes = [ "/var/lib/zwave-js-ui/store:/usr/src/app/store" ];
       extraOptions = [ "--device=/dev/serial/by-id/usb-Silicon_Labs_HubZ_Smart_Home_Controller_61200B7D-if00-port0:/dev/zwave" ];
