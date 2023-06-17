78a5d935e929feff98a95dedf3247aa143656487
Morgan Helton <mhelton@gmail.com>
2023-06-17 13:33:07 -0500

    update flake.lock

--- overlay/default.nix
+++ overlay/default.nix
@@ -5,10 +5,10 @@
 
   plexpass = prev.plex.override {
     plexRaw = prev.plexRaw.overrideAttrs (old: rec {
-      version = "1.32.1.6983-93784ef3f";
+      version = "1.32.4.7195-7c8f9d3b6";
       src = prev.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "sha256-cNEKclXAPBqU8V5/A71pRQbPwRlrzsexsuJGAgKEals=";
+        sha256 = "sha256-lnkhjzBW9DENCVJNP+4h8fVVUh4LhnJ1XjrCjF3ngKE=";
       };
     });
   };
