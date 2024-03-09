1b209dd4d654e28d0d956ed0d85a2cfa1f2abf5a
Morgan Helton <mhelton@gmail.com>
2024-03-09 08:33:52 -0600

    add chopper second NIC as bridge interface

--- hosts/chopper/virtualisation.nix
+++ hosts/chopper/virtualisation.nix
@@ -1,8 +1,22 @@
 { ... }: {
   virtualisation.libvirtd = {
     enable = true;
+    allowedBridges = [
+      "virbr0"
+    ];
   };
 
   users.users.mhelton.extraGroups = [ "libvirtd" ];
+
+  networking = {
+    interfaces.enp5s0.useDHCP = false;
+    bridges = {
+      virbr0 = {
+        interfaces = [
+          "enp5s0"
+        ];
+      };
+    };
+  };
 
 }
