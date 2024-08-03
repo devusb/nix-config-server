7a56438685ee2aafb52736c056ef3d852c28aa25
Morgan Helton <mhelton@gmail.com>
2024-08-03 09:50:14 -0500

    chopper: re-implement virt bridge with networkd

--- hosts/chopper/virtualisation.nix
+++ hosts/chopper/virtualisation.nix
@@ -2,21 +2,33 @@
   virtualisation.libvirtd = {
     enable = true;
     allowedBridges = [
-      "virbr0"
+      "br0"
     ];
   };
 
   users.users.mhelton.extraGroups = [ "libvirtd" ];
 
-  networking = {
-    interfaces.enp5s0.useDHCP = false;
-    bridges = {
-      virbr0 = {
-        interfaces = [
-          "enp5s0"
-        ];
-      };
+  systemd.network.networks."20-lan" = {
+    matchConfig.Name = ["enp5s0" "vm-*"];
+    networkConfig = {
+      Bridge = "br0";
     };
+  };
+
+  systemd.network.netdevs."br0" = {
+    netdevConfig = {
+      Name = "br0";
+      Kind = "bridge";
+    };
+  };
+
+  systemd.network.networks."20-lan-bridge" = {
+    matchConfig.Name = "br0";
+    networkConfig = {
+      DHCP = "yes";
+    };
+    dhcpV4Config.RouteMetric = 2000;
+    linkConfig.RequiredForOnline = "routable";
   };
 
 }
