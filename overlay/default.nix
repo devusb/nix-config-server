5ecab9993ed847d2ab6bc6b8bb9738401e64b976
Morgan Helton <mhelton@gmail.com>
2024-11-05 14:53:48 -0600

    overlays: shairport-sync 4.3.4-unstable-2024-11-02

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,9 +6,19 @@
 
   caddy-cloudflare = prev.callPackage ./caddy-cloudflare.nix { };
 
-  shairport-sync = prev.shairport-sync.override {
-    enableAirplay2 = true;
-  };
+  shairport-sync =
+    let
+      shairport-sync' = prev.shairport-sync.overrideAttrs (old: {
+        version = "4.3.4-unstable-2024-11-02";
+        src = old.src.override {
+          rev = "ab6225c1ac1c57f5af50890d722437ec8a921d0d";
+          hash = "sha256-iwyIUUFA5DzTkm/DXvEa3buVX4Dje0P0svteRAKIS20=";
+        };
+      });
+    in
+    shairport-sync'.override {
+      enableAirplay2 = true;
+    };
 
   plexpass = prev.plex.override {
     plexRaw = prev.plexRaw.overrideAttrs (old: rec {
