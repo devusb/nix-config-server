d6b6e6564000d127629f97e5872c024609340915
Morgan Helton <mhelton@gmail.com>
2024-12-14 13:27:14 -0600

    chopper: permit insecure sonarr deps

--- flake.nix
+++ flake.nix
@@ -96,6 +96,13 @@
               config = {
                 allowUnfree = true;
                 nvidia.acceptLicense = true;
+                permittedInsecurePackages = [
+                  # https://github.com/NixOS/nixpkgs/issues/360592
+                  "aspnetcore-runtime-6.0.36"
+                  "aspnetcore-runtime-wrapped-6.0.36"
+                  "dotnet-sdk-6.0.428"
+                  "dotnet-sdk-wrapped-6.0.428"
+                ];
               };
             };
             _module.args.pkgs = legacyPackages;
