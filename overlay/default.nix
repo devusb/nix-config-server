79e0c8b9da4398601024be26f7ebce4606f19622
Morgan Helton <mhelton@gmail.com>
2026-01-17 16:26:49 -0600

    chore: drop memos overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -21,34 +21,6 @@ final: prev: {
     doCheck = false;
   };
 
-  memos = prev.memos.overrideAttrs (old: rec {
-    version = "0.25.3";
-    src = old.src.override {
-      rev = "v${version}";
-      hash = "sha256-lAKzPteGjGa7fnbB0Pm3oWId5DJekbVWI9dnPEGbiBo=";
-    };
-
-    vendorHash = "sha256-BoJxFpfKS/LByvK4AlTNc4gA/aNIvgLzoFOgyal+aF8";
-
-    preBuild =
-      let
-        memos-web = old.memos-web.overrideAttrs {
-          inherit src;
-          pnpmDeps = prev.pnpm.fetchDeps {
-            inherit (old) pname;
-            inherit version src;
-            sourceRoot = "${src.name}/web";
-            fetcherVersion = 1;
-            hash = "sha256-k+pykzAiZ72cMMH+6qtnNxjaq4m4QyCQuWvQPbZSZho";
-          };
-        };
-      in
-      ''
-        rm -rf server/router/frontend/dist
-        cp -r ${memos-web} server/router/frontend/dist
-      '';
-  });
-
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (self: super: {
       flask-limiter = super.flask-limiter.overridePythonAttrs (old: {
