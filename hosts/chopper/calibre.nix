8607f1c9c81e623d7a7c467e6cadf18549cdab61
Morgan Helton <mhelton@gmail.com>
2026-02-07 13:59:54 -0600

    chore(hosts/chopper/calibre): swap to calibre-web-automated module

--- hosts/chopper/calibre.nix
+++ hosts/chopper/calibre.nix
@@ -3,20 +3,11 @@
   ...
 }:
 {
-  virtualisation.oci-containers.containers.calibre-web-automated = {
-    image = "crocodilestick/calibre-web-automated:v4.0.6";
-    environment = {
-      NETWORK_SHARE_MODE = "false";
-      TZ = "US/Central";
-    };
-    volumes = [
-      "/var/lib/calibre-web:/config"
-      "/var/lib/calibre-web:/calibre-library"
-      "/r2d2_0/media/Books:/r2d2_0/media/Books"
-    ];
-    ports = [
-      "8083:8083"
-    ];
+  services.calibre-web-automated = {
+    enable = true;
+    listen.ip = "127.0.0.1";
+    dataDir = "calibre-web";
+    options.calibreLibrary = "/var/lib/calibre-web";
   };
 
   services.caddy.virtualHosts = with caddyHelpers; {
