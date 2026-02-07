e0828223e11128dd0e455fb7be89facfa6add026
Morgan Helton <mhelton@gmail.com>
2026-02-07 15:42:07 -0600

    chore(hosts/chopper/calibre): add jump to KOReader patch

--- hosts/chopper/calibre.nix
+++ hosts/chopper/calibre.nix
@@ -1,10 +1,19 @@
 {
   caddyHelpers,
+  pkgs,
   ...
 }:
 {
   services.calibre-web-automated = {
     enable = true;
+    package = pkgs.calibre-web-automated.overrideAttrs (old: {
+      patches = old.patches ++ [
+        (pkgs.fetchpatch {
+          url = "https://github.com/crocodilestick/Calibre-Web-Automated/compare/main...devusb:Calibre-Web-Automated:jump-to-koreader.patch";
+          hash = "sha256-kp48O2lVcG8+qQ222RfNbuqTIxJKzey4IzVyxN5rkqk=";
+        })
+      ];
+    });
     listen.ip = "127.0.0.1";
     dataDir = "calibre-web";
     options.calibreLibrary = "/var/lib/calibre-web";
