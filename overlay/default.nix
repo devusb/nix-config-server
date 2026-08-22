e7c5316722b6e915d85a89c49bfc08379fc1a6da
Morgan Helton <mhelton@gmail.com>
2026-08-22 07:06:49 -0500

    chore(overlays): drop stump patches

--- overlay/default.nix
+++ overlay/default.nix
@@ -7,22 +7,6 @@ final: prev: {
 
   makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
 
-  stump = prev.stump.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      (prev.fetchpatch {
-        # koreader progress pull returning 500
-        url = "https://github.com/devusb/stump/commit/6413ef2dc2e77cdf2840e79133b8f7269cf6069f.patch";
-        hash = "sha256-qKRF0XXK7MFA/47SmNdcuvKvlCFErL8cJUzR9mm3R28=";
-      })
-      (prev.fetchpatch {
-        # fix oidc migration
-        url = "https://github.com/stumpapp/stump/commit/e95b9218149f3597e809d8581d12088571f5e366.patch";
-        hash = "sha256-zHHdzAkVxwoPBDKONrr+1XHTlqU+B4713WkRIvJTBIA=";
-      })
-    ];
-    doCheck = false;
-  });
-
   plexpass = prev.plex.override {
     plexRaw = prev.plexRaw.overrideAttrs (old: rec {
       version = "1.41.3.9314-a0bfb8370";
